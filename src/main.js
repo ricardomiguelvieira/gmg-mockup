bcb97ab5f08fe10736e8b1736110bb28b2b76de4
Ricardo Vieira <ricmvieira@gmail.com>
2019-03-27 13:39:23 +0000

    added favicon

--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@ import Vue from 'vue';
 import App from './app.vue';
 import router from '../router';
 import BootstrapVue from 'bootstrap-vue'
-import VueLodash from 'vue-lodash'
 import axios from 'axios'
-import VueAxios from 'vue-axios'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
-Vue.use(require('vue-faker'));
-Vue.use(VueLodash);
-Vue.use(VueAxios, axios)
+Vue.use(axios)
 
 const app = new Vue({
   router,
