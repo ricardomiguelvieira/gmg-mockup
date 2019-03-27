6ecd3b90a63e825f5101e78697271737a3360f7e
Ricardo Vieira <ricmvieira@gmail.com>
2019-03-27 08:39:36 +0000

    styled team page

--- views/team.vue
+++ views/team.vue
@@ -9,6 +9,9 @@
             <h4>Research Assistant</h4>
             <p>Becky (BA, PGCE) completed her undergraduate degree in Education at the University of York before working as a primary school teacher on the Teach First programme. She is looking forward to beginning an MPhil in Educational Research at the University of Cambridge in October.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/ravi.png" img-alt="Card image" img-left class="mb-3">
@@ -17,66 +20,119 @@
             <h4>Front-End Developer</h4>
             <p>Ravi has a passion for web development and enjoys having the responsibility for the creation and maintenance of the Get My Grades user interfaces.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/male_user.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Ross Hulford</h3>
+            <h4>Senior Developer</h4>
+            <p>Ross is a talented web developer with over 12 years’ solid commercial experience. He enjoys keeping ahead of the technology curve and using the latest features and enhancements to improve our user experience.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/male_user.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Jonathan Kingston-Fear</h3>
+            <h4>Senior Developer</h4>
+            <p>Jonathan is a self­-confessed computer geek with a passion for the latest technology. He enjoys building scalable web applications, optimised for fast loading times and user experience.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/arthur.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Arthur Nichols</h3>
+            <h4>Big Boss</h4>
+            <p>Arthur is our ‘Big Boss’. He oversees the work of all the team, making the important executive decisions that nobody else wants to make, like, who needs more biscuits, or, can we spin in the spinny-chairs?</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/john.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>John Nichols</h3>
+            <h4>Head of Education/Co-Founder</h4>
+            <p>John (BSc, PGCE, MTA) earned a PGCE with Distinction on the Teach First Programme as a science teacher. He has thousands of hours of private tuition experience and wants to change the education world!</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/bryden.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Bryden Commons</h3>
+            <h4>Head of SEN and Inclusion</h4>
+            <p>Bryden (BBSc) has a degree in Behavioural Science and is trained in Applied Behavioural Analysis. She is passionate about creating accessible education for young people with special educational needs or mental illness.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/charlotte.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Charlotte Forrest</h3>
+            <h4>Head of Research</h4>
+            <p>Charlotte (MA, MSc, PhD) has a PhD in Psychology and over 10 years experience of researching learning and memory. She is bringing her knowledge and skills learnt from 3 of the top 15 departments in the country to create the best learning environment for students.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/male_user.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Titus Todd</h3>
+            <h4>Maths Content Creator</h4>
+            <p>Titus (MA, PGCE) graduated with a Classics degree from the University of St Andrews. He now works as the Second in Maths at his school having completed the Teach First Leadership Development programme.</p>
           </b-card-text>
+          <div id="email-icon">
+            <a href="#"><img src="../src/assets/images/email_icon.png"></img></a>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/female_user.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Natalie Jones</h3>
+            <h4>Maths Content Creator</h4>
+            <p>Natalie (BSc, PGCE) holds a first class degree in Mathematics and a PGCE. She is the CPD lead in her mathematics department and she believes that sequential curriculum design is vital for the success of pupils.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/jenn.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Jenn Linning</h3>
+            <h4>Head of English</h4>
+            <p>Jenn (MA, PGCE, CELTA) has a Master's degree in English Language and gained a PGCE (with distinction) on the Teach First Programme. Jenn specialises in English as an Additional Language and is CELTA qualified.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
 
         <b-card img-src="../src/assets/images/samantha.jpg" img-alt="Card image" img-left class="mb-3">
           <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
+            <h3>Samantha Nichols</h3>
+            <h4>Managing Director/Co-Founder</h4>
+            <p>Sam (BSc) spent much of her early career working as a personal assistant, sales and finance executive, and operations manager. She brings her many administrative talents and skills to help everything run smoothly.</p>
           </b-card-text>
+          <div id="email-icon">
+            <img src="../src/assets/images/email_icon.png"></img>
+          </div>
         </b-card>
       </div>
     </div>
@@ -100,8 +156,12 @@ export default {
 <style>
 
 .card-img-left {
-  width: auto;
   height: 200px;
+}
+
+#email-icon img {
+  height: 24px;
+  vertical-align: bottom;
 }
 
 </style>
@@ -140,7 +200,7 @@ export default {
     font-size: 1rem;
     font-family: 'Ubuntu', sans-serif;
     font-style: italic;
-    font-weight: 200;
+    font-weight: 400;
   }
 
   p {
